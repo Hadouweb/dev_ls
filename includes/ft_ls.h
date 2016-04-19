f1ee246784cf6a14226abd7a6cefc86a5dcea134
hadouweb <kengo14@hotmail.fr>
2016-04-19 17:59:37 +0200

    test readdir en cours

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -2,6 +2,9 @@
 # define FT_LS_H
 
 # include "libft.h"
+# include <sys/types.h>
+# include <dirent.h>
+# include <errno.h>
 # include <stdio.h>
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,31 @@
 #include "ft_ls.h"
 
-int		main(void)
+DIR		*ft_opendir(char *file)
 {
-	ft_putstr("OK\n");
+	DIR 	*dir;
+
+	errno = 0;
+	dir = opendir(file);
+	if (errno != 0)
+		perror("Error in ft_opendir ");
+	return (dir);
+}
+
+void	ft_readdir(DIR *dir)
+{
+	struct dirent *dp;
+
+	dp = readdir(dir);
+}
+
+int		main(int ac, char **av)
+{
+	DIR		*dir;
+
+	if (ac > 1)
+	{
+		dir = ft_opendir(av[1]);
+		ft_readdir(dir);
+	}
 	return (0);
 }
