9aca3795a4f247a19fd1821f61afde091c05118c
Hadouweb <kengo14@hotmail.fr>
2016-10-24 16:58:29 +0200

    Update libft abd progress ls

--- src/ls_set_data.c
+++ src/ls_set_data.c
@@ -20,7 +20,7 @@ void	debug_print_content(void *content)
 	ft_putstr(path->name);
 }
 
-t_listd	*ls_get_node_cmp(t_listd **lst, t_path *path)
+t_listd	*ls_get_node_cmp_time(t_listd **lst, t_path *path)
 {
 	__uint128_t 	t1;
 	__uint128_t 	t2;
@@ -39,24 +39,19 @@ t_listd	*ls_get_node_cmp(t_listd **lst, t_path *path)
 			t2 = (__uint128_t) path_cmp->file.st_mtimespec.tv_sec
 						 << 64 | path_cmp->file.st_mtimespec.tv_nsec;
 
-			if (t2 - t1 == 0 && ft_strcmp(path->name, path_cmp->name) > 0)
+			if (t2 - t1 == 0 && ft_strcmp(path->name, path_cmp->name) < 0)
 			{
-				printf("t1:[%s] = t2:[%s]\n", path->name, path_cmp->name);
-				if (!S_ISDIR(path->file.st_mode) &&
-					S_ISDIR(path_cmp->file.st_mode))
-				{
-					printf("not dir:[%s] dir:[%s]\n", path->name, path_cmp->name);
-					return l;
-				}
+				//printf("t1:[%s] = t2:[%s]\n", path->name, path_cmp->name);
+				return l;
 			}
-			printf("t1:[%s][%ld %ld] t2[%s][%ld %ld]\n",
+			/*printf("t1:[%s][%ld %ld] t2[%s][%ld %ld]\n",
 				   path->name, path->file.st_mtimespec.tv_sec,
 				   path->file.st_mtimespec.tv_nsec,
 				   path_cmp->name, path_cmp->file.st_mtimespec.tv_sec,
-				   path_cmp->file.st_mtimespec.tv_nsec);
+				   path_cmp->file.st_mtimespec.tv_nsec);*/
 			if (t1 > t2)
 			{
-				printf("t1:[%s] > t2[%s]\n", path->name, path_cmp->name);
+				//printf("t1:[%s] > t2[%s]\n", path->name, path_cmp->name);
 				return l;
 			}
 		}
@@ -65,21 +60,49 @@ t_listd	*ls_get_node_cmp(t_listd **lst, t_path *path)
 	return NULL;
 }
 
+t_listd	*ls_get_node_cmp(t_listd **lst, t_path *path)
+{
+	t_listd 		*l;
+	t_path 			*path_cmp;
+
+	l = *lst;
+	while (l)
+	{
+		path_cmp = (t_path*)l->content;
+		if (ft_strcmp(path->name, path_cmp->name) != 0)
+		{
+			/*if (!ft_isalpha(path->name[0]) && ft_isalpha(path_cmp->name[0]))
+				return l;
+			if (ft_isupper(path->name[0]) && !ft_isupper(path_cmp->name[0]))
+				return l;
+			if (((ft_isupper(path->name[0]) && ft_isupper(path_cmp->name[0])) ||
+					(ft_islower(path->name[0]) && ft_islower(path_cmp->name[0]))) &&
+					ft_strcmp(path->name, path_cmp->name) < 0)
+				return l;*/
+			if (ft_strcmp(path->name, path_cmp->name) < 0)
+				return l;
+		}
+		l = l->next;
+	}
+	return NULL;
+}
 
 void	ls_push_after_sort(t_listd **lst, t_path *path, t_app *app)
 {
+	t_listd *node;
+
+	node = NULL;
 	if (app->opt & OPT_t)
+		node = ls_get_node_cmp_time(lst, path);
+	else
+		node = ls_get_node_cmp(lst, path);
+	if (node != NULL)
 	{
-		t_listd *node = ls_get_node_cmp(lst, path);
-		if (node != NULL)
-		{
-			ft_lstd_pushbefore_node(lst, node, ft_lstd_new((void *) path, sizeof(t_path)));
-			printf("before this: %s current: %s\n",
-			 ((t_path*)node->content)->name, path->name);
-		} else {
-			ft_lstd_pushback(lst, (void *) path, sizeof(t_path));
-			printf("add classic: %s\n", path->name);
-		}
+		ft_lstd_pushbefore_node(lst, node, ft_lstd_new((void *) path, sizeof(t_path)));
+		//printf("before this: %s current: %s\n", ((t_path*)node->content)->name, path->name);
+	} else {
+		ft_lstd_pushback(lst, (void *) path, sizeof(t_path));
+		//printf("add classic: %s\n", path->name);
 	}
 }
 
@@ -88,7 +111,7 @@ void	ls_set_file_data(char *rpath, char *name, t_listd **lst, t_app *app)
 	t_path	*path;
 	int		ret;
 
-	ft_lstd_print(*lst, debug_print_content, 1);
+	//ft_lstd_print(*lst, debug_print_content, 1);
 	//printf("ls_set_file_data | rpath: %s name: %s\n", rpath, name);
 
 	ret = -1;
